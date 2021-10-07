c3a109ac843c438fb7ef3995c47f810181e3de9a
vuthibichloan <92076988+vuthibichloan@users.noreply.github.com>
2021-10-07 14:17:12 +0700

    Add files via upload

---
+++ DangnhapBUS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLDiemTHPT.Entities;
+using QLDiemTHPT.DataAccess;
+
+namespace QLDiemTHPT.Bussiness
+{
+    class DangnhapBUS
+    {
+        DangnhapDAO usdao = new DangnhapDAO(@"Data Source=DESKTOP-KBSLCQ1;Initial Catalog=DoAn_QuanlyDiemHSTHPT;Integrated Security=True");
+
+        public DangnhapBUS() { }
+
+        public DangnhapBUS(string stcon)
+        {
+            usdao = new DangnhapDAO(stcon);
+        }
+
+        public Users CheckUser(string tentk, string mk)
+        {
+            return usdao.GetUsers(tentk, mk);
+        }
+    }
+}
---
+++ GiaovienBUS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLDiemTHPT.Entities;
+using QLDiemTHPT.DataAccess;
+
+namespace QLDiemTHPT.Bussiness
+{
+    class GiaovienBUS
+    {
+    }
+}
